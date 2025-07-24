a53f225394f025de0e00a1e2e612e3009474a0f2
wildanhawari <wildanhawari702@gmail.com>
2025-07-24 16:44:36 +0700

    prisma

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -254,7 +254,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
